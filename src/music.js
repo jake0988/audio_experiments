a264a8c1682ef8f1c5760caf80fcf346fea86392
Jake <jake098877@gmail.com>
2022-03-10 15:23:04 -0500

    First commit.

---
+++ src/music.js
@@ -0,0 +1,12 @@
+import * as Tone from "tone";
+
+const synth = new Tone.Synth().toMaster();
+synth.triggerAttackRelease("C4", "8n");
+
+document.getElementById("play-button").addEventListener("click", function () {
+  if (Tone.Transport.state !== "started") {
+    Tone.start();
+  } else {
+    Tone.Transport.stop();
+  }
+});
